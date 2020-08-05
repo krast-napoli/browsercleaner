43cba4bf94cbd978e8dbf3b201a7620ff29f5470
krast-napoli <44581068+krast-napoli@users.noreply.github.com>
2020-08-05 08:09:09 +0300

    Add GUI

--- simplecleaner.py
+++ simplecleaner.py
@@ -1,4 +1,11 @@
 import os
+from tkinter import *
+
+root = Tk()
+
+e = Entry(width=20)
+b = Button (text="Change")
+l = Label(bg='black', fg='white', width=20)
 
 username = os.getlogin()
 
@@ -6,4 +13,16 @@ path1 = r"C:\Users"
 path2 = r"AppData\Local\Google\Chrome\User Data\Default\History"
 chromepath = path1 + '\\' + '\\' + path2
 
-os.remove (chromepath)
+def clean_history(event):
+    os.remove (chromepath)
+
+
+b.bind('<Button-1>', clean_history)
+
+e.pack()
+b.pack()
+l.pack()
+root.mainloop()
+
+
+
